90d9a079ff2e5708d926d6caa3b46dd0fbf14a21
Oscar <159458439+05c4rR@users.noreply.github.com>
2024-09-04 14:32:43 +0200

    Users update + Users.php + admin dashboard + login form

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -10,7 +11,7 @@ class IndexController extends AbstractController
 {
     #[Route('/', name: 'home')]
     public function index(): Response
-    {
+    {   
         return $this->render('index/index.html.twig', [
             'controller_name' => 'IndexController',
             'page_name'       => 'Accueil',
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -7,13 +7,19 @@ use App\Entity\Product;
 use App\Entity\SpecialOffer;
 use App\Entity\Stock;
 use App\Entity\Tax;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
     private const NB_PRODUCTS = 100;
+
+    public function __construct(
+        private UserPasswordHasherInterface $hasher
+    ) {}
 
     public function load(ObjectManager $manager): void
     {
@@ -75,6 +81,21 @@ class AppFixtures extends Fixture
             
             $manager->persist($product);
         }
+
+        $admin = new User;
+        $admin
+            ->setEmail('admin@test.com')
+            ->setPassword($this->hasher->hashPassword($admin, 'admin'))
+            ->setRoles(['ROLE_ADMIN']);
+
+        $manager->persist($admin);
+
+        $user = new User;
+        $user
+            ->setEmail('user@test.com')
+            ->setPassword($this->hasher->hashPassword($user, 'user'));
+
+        $manager->persist($user);
 
         $manager->flush();
     }
